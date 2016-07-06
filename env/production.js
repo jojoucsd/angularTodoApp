c0f505a948a2ddaa7ea488194158560eebe76c37
Ling Lu <jojoucsd@gmail.com>
2016-07-06 14:46:46 -0700

    changed Mongolab to MongoDB

--- env/production.js
+++ env/production.js
@@ -2,7 +2,7 @@ var port = process.env.PORT;
 
 module.exports = {
   port: port,
-  db: process.env.MONGOLAB_URI || process.env.MONGOHQ_URI || 'mongodb://localhost/d2rAngular',
+  db: process.env.MONGODB_URI || process.env.MONGODB_URI || 'mongodb://heroku_j36tb2km:8211sa0l8ijti2vic56mgm5ttt@ds011409.mlab.com:11409/heroku_j36tb2km',
   TOKEN_SECRET: process.env.TOKEN_SECRET,
   FACEBOOK_SECRET: process.env.FACEBOOK_SECRET,
 };
